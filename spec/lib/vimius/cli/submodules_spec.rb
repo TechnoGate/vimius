0a3ecdab5b2e83f06f6da612451f0117d1869009
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-14 04:45:35 +0100

    Cli/Submodules spec: Add pending specs (plans).

--- spec/lib/vimius/cli/submodules_spec.rb
+++ spec/lib/vimius/cli/submodules_spec.rb
@@ -23,12 +23,34 @@ module CLI
         returns({"submodules" => ["pathogen", "tlib", "github"]}.with_indifferent_access)
     end
 
-    context "list" do
-      it { should respond_to :list }
-
-      it "should print the list of submodules organised by group" do
-        subject.list
+    context "List" do
+      context "by group" do
+        it "should be able to list submodules by group"
+        it "should mark activated submodules with an asterisk"
+        it "should add 2 spaces before inactive submodules"
       end
+
+      context "by name" do
+        it "should be able to list submodules sorted by name"
+        it "should mark activated submodules with an asterisk"
+        it "should add 2 spaces before inactive submodules"
+      end
+    end
+
+    context "Activate" do
+      it "should be able to activate a submodule"
+      it "should print an error if the submodule is already activated"
+      it "should print an error if the submodule does not exit"
+      it "should update the config file"
+      it "should be listed as active"
+    end
+
+    context "Deactivate" do
+      it "should be able to deactivate a submodule"
+      it "should print an error if the submodule is already deactivated"
+      it "should print an error if the submodule does not exit"
+      it "should update the config file"
+      it "should be listed as inactive"
     end
   end
 end

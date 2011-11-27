6a8e6009874eb597b2e2edfa2af4ecf99ccecddd
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-27 16:21:22 +0100

    Install: Take advantage of the Base workflow to refactor the install and its specs.

--- lib/vimius/command/install.rb
+++ lib/vimius/command/install.rb
@@ -9,12 +9,6 @@ module TechnoGate
 
         register "install", "Install Janus"
 
-        def execute
-          sanity_check
-          Shell.exec("git clone -b #{REPO_BRANCH} #{REPO_URL}")
-        end
-
-        protected
         def sanity_check
           if ::File.exists?(USER_VIM_PATH)
             abort "#{USER_VIM_PATH} exists, cannot continue, please run 'vimius setup' instead."
@@ -28,74 +22,9 @@ module TechnoGate
             abort "#{USER_GVIMRC_PATH} exists, cannot continue, please run 'vimius setup' instead."
           end
         end
-      end
-    end
-  end
-end
 
-
-if false
-  require 'fileutils'
-
-  module TechnoGate
-    module Vimius
-      module CLI
-        module Install
-
-          def self.included(base)
-            base.send :require, 'open-uri'
-            base.class_eval <<-END, __FILE__, __LINE__ + 1
-              desc "install", "Install vimius"
-              def install
-                # Sanity check
-                sanity_check
-
-                # Clone the repository
-                Shell.exec("git clone git://github.com/TechnoGate/vimius.git ~/.vim")
-
-                # Create symlinks
-                FileUtils.ln_s(File.join(USER_VIM_PATH, 'vimius', 'vimrc'), USER_VIMRC_PATH)
-                FileUtils.ln_s(File.join(USER_VIM_PATH, 'vimius', 'gvimrc'), USER_GVIMRC_PATH)
-              end
-
-              desc "setup", "Move away any non-vimius installation and install vimius"
-              def setup
-                # Make way for Vimius
-                make_way_for_vimius
-                # Call the install task
-                install
-              end
-
-              protected
-              def sanity_check
-                if File.exists?(USER_VIM_PATH)
-                  abort "\#{USER_VIM_PATH} exists, cannot continue, please run 'vimius setup' instead."
-                end
-
-                if File.exists?(USER_VIMRC_PATH)
-                  abort "\#{USER_VIMRC_PATH} exists, cannot continue, please run 'vimius setup' instead."
-                end
-
-                if File.exists?(USER_GVIMRC_PATH)
-                  abort "\#{USER_GVIMRC_PATH} exists, cannot continue, please run 'vimius setup' instead."
-                end
-              end
-
-              def make_way_for_vimius
-                if File.exists?(USER_VIM_PATH)
-                  FileUtils.mv USER_VIM_PATH, "\#{USER_VIM_PATH}.old"
-                end
-
-                if File.exists?(USER_VIMRC_PATH)
-                  FileUtils.mv USER_VIMRC_PATH, "\#{USER_VIMRC_PATH}.old"
-                end
-
-                if File.exists?(USER_GVIMRC_PATH)
-                  FileUtils.mv USER_GVIMRC_PATH, "\#{USER_GVIMRC_PATH}.old"
-                end
-              end
-            END
-          end
+        def execute
+          Shell.exec("git clone -b #{REPO_BRANCH} #{REPO_URL}")
         end
       end
     end
--- spec/lib/vimius/command/install_spec.rb
+++ spec/lib/vimius/command/install_spec.rb
@@ -7,24 +7,6 @@ module Command
 
     let(:install) { TechnoGate::Vimius::Command::Install.new }
 
-    context "#install" do
-      it "should have a task called install" do
-        subject.tasks.should include "install"
-      end
-
-      it "should call sanity_check" do
-        install.class.any_instance.expects(:sanity_check).at_least(1)
-
-        subject.start(["install"])
-      end
-
-      it "should clone the repositoru" do
-        Shell.expects(:exec).with("git clone -b #{REPO_BRANCH} #{REPO_URL}")
-
-        subject.start(["install"])
-      end
-    end
-    
     context '#sanity_check' do
       before(:each) do
         ::File.stubs(:exists?).with(USER_GVIMRC_PATH).returns(false)
@@ -37,24 +19,21 @@ module Command
         it "should check that USER_VIM_PATH exists" do
           ::File.expects(:exists?).with(USER_VIM_PATH).returns(true).once
 
-          install.send(:sanity_check).should be_false
-          install.send(:sanity_check).
+          subject.start(["install"]).
             should puts("#{USER_VIM_PATH} exists, cannot continue, please run 'vimius setup' instead.")
         end
 
         it "should check that USER_VIMRC_PATH exists" do
           ::File.expects(:exists?).with(USER_VIMRC_PATH).returns(true).once
 
-          install.send(:sanity_check).should be_false
-          install.send(:sanity_check).
+          subject.start(["install"]).
             should puts("#{USER_VIMRC_PATH} exists, cannot continue, please run 'vimius setup' instead.")
         end
 
         it "should check that USER_GVIMRC_PATH exists" do
           ::File.expects(:exists?).with(USER_GVIMRC_PATH).returns(true).once
 
-          install.send(:sanity_check).should be_false
-          install.send(:sanity_check).
+          subject.start(["install"]).
             should puts("#{USER_GVIMRC_PATH} exists, cannot continue, please run 'vimius setup' instead.")
         end
       end
@@ -62,114 +41,21 @@ module Command
       context 'vim is not already installed' do
 
         it "should not output anything" do
-          install.send(:sanity_check).should puts(nil)
+          subject.start(["install"]).should puts(nil)
         end
+      end
+    end
+
+    context "#install" do
+      it "should have a task called install" do
+        subject.tasks.should include "install"
+      end
+
+      it "should clone the repositoru" do
+        Shell.expects(:exec).with("git clone -b #{REPO_BRANCH} #{REPO_URL}")
+
+        subject.start(["install"])
       end
     end
   end
 end
-
-#require 'spec_helper'
-
-#class CliInstallTestClass < ::Thor
-  #include CLI::Install
-#end
-
-#module CLI
-  #describe Install do
-    #subject { CliInstallTestClass.new }
-
-    #context "#install" do
-      #pending { should respond_to :install }
-
-      #pending "should call sanity_check" do
-        #subject.expects(:sanity_check).once
-
-        #subject.install
-      #end
-
-      #pending "should call Shell.exec" do
-        #Shell.expects(:exec).with("gpending clone git://github.com/TechnoGate/vimius.gpending ~/.vim")
-
-        #subject.install
-      #end
-    #end
-
-    #context "#setup" do
-      #pending { should respond_to :setup }
-
-      #pending "should call :make_way_for_vimius" do
-        #subject.expects(:make_way_for_vimius).once
-
-        #subject.setup
-      #end
-
-      #pending "should call install" do
-        #subject.expects(:install).once
-
-        #subject.setup
-      #end
-    #end
-
-    #context "#make_way_for_vimius" do
-      #pending { should respond_to :make_way_for_vimius }
-
-      #pending "should move any vim file before running" do
-        #::FileUtils.expects(:mv).with(USER_VIM_PATH, "#{USER_VIM_PATH}.old").once
-        #::FileUtils.expects(:mv).with(USER_VIMRC_PATH, "#{USER_VIMRC_PATH}.old").once
-        #::FileUtils.expects(:mv).with(USER_GVIMRC_PATH, "#{USER_GVIMRC_PATH}.old").once
-
-        #subject.send :make_way_for_vimius
-      #end
-    #end
-
-    #context '#sanity_check' do
-      #before(:each) do
-        #::File.stubs(:exists?).with(USER_GVIMRC_PATH).returns(true)
-        #::File.stubs(:exists?).with(USER_VIMRC_PATH).returns(true)
-        #::File.stubs(:exists?).with(USER_VIM_PATH).returns(true)
-      #end
-
-      #pending "should check that USER_VIM_PATH exists" do
-        #::File.expects(:exists?).with(USER_VIM_PATH).returns(true).once
-
-        #subject.send :sanity_check
-      #end
-
-      #pending "should check that USER_VIMRC_PATH exists" do
-        #::File.expects(:exists?).with(USER_VIMRC_PATH).returns(true).once
-
-        #subject.send :sanity_check
-      #end
-
-      #pending "should check that USER_GVIMRC_PATH exists" do
-        #::File.expects(:exists?).with(USER_GVIMRC_PATH).returns(true).once
-
-        #subject.send :sanity_check
-      #end
-
-      #context 'failure' do
-        #pending "should abort if USER_VIM_PATH exists." do
-          #::File.expects(:exists?).with(USER_VIM_PATH).returns(false).once
-
-          #subject.send(:sanity_check).should be_false
-          #"#{USER_VIM_PATH} exists, cannot continue, please run 'vimius setup' instead.".should be_in_output
-        #end
-
-        #pending "should abort if USER_VIMRC_PATH exists." do
-          #::File.expects(:exists?).with(USER_VIMRC_PATH).returns(false).once
-
-          #subject.send(:sanity_check).should be_false
-          #"#{USER_VIMRC_PATH} exists, cannot continue, please run 'vimius setup' instead.".should be_in_output
-        #end
-
-        #pending "should abort if USER_GVIMRC_PATH exists." do
-          #::File.expects(:exists?).with(USER_GVIMRC_PATH).returns(false).once
-
-          #subject.send(:sanity_check).should be_false
-          #"#{USER_GVIMRC_PATH} exists, cannot continue, please run 'vimius setup' instead.".should be_in_output
-        #end
-      #end
-    #end
-  #end
-#end

dc6c343a74c81631b0e782171a0895e6adcc82f2
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-27 16:08:01 +0100

    SubmodulesCommand: Finish it up.

--- spec/lib/vimius/command/submodules_spec.rb
+++ spec/lib/vimius/command/submodules_spec.rb
@@ -28,19 +28,72 @@ Active submodules are prefixed with the '|==' marker
     end
 
     context "#activate" do
-      it "should be able to activate a submodule"
-      it "should print an error if the submodule is already activated"
-      it "should print an error if the submodule does not exit"
-      it "should update the config file"
-      it "should be listed as active"
+      it "should be able to activate a submodule" do
+        subject.start ["submodules", "activate", "command-t"]
+
+        Vimius.submodules.active?("command-t").should be_true
+      end
+
+      it "should print a message when done" do
+        subject.start(["submodules", "activate", "command-t"]).
+          should puts "command-t has been activated please run 'vimius update'"
+      end
+
+      it "should print an error if the submodule is already activated" do
+        subject.start(["submodules", "activate", "pathogen"]).
+          should puts "pathogen is already active"
+      end
+
+      it "should print an error if the submodule does not exit" do
+        subject.start(["submodules", "activate", "invalid-submodule"]).
+          should puts "invalid-submodule does not exist"
+      end
     end
 
     context "#deactivate" do
-      it "should be able to deactivate a submodule"
-      it "should print an error if the submodule is already deactivated"
-      it "should print an error if the submodule does not exit"
-      it "should update the config file"
-      it "should be listed as inactive"
+      it "should be able to deactivate a submodule" do
+        subject.start ["submodules", "deactivate", "github"]
+
+        Vimius.submodules.active?("github").should be_false
+      end
+
+      it "should print a message when done" do
+        subject.start(["submodules", "deactivate", "github"]).should puts <<-EOM
+The following submodules has been deactivated, please run 'vimius update'
+- github
+        EOM
+      end
+
+      it "should print an error if the submodule is already deactivated" do
+        subject.start(["submodules", "deactivate", "command-t"]).
+          should puts "command-t is not active"
+      end
+
+      it "should print an error if the submodule does not exit" do
+        subject.start(["submodules", "deactivate", "invalid-submodule"]).
+          should puts "invalid-submodule does not exist"
+      end
+
+      it "should print an error if the submodule is depended on" do
+        subject.start(["submodules", "deactivate", "pathogen"]).should puts <<-EOM
+pathogen cannot be deactivated because it is depended on by:
+- command-t
+- github
+- tlib
+
+Please re-run with the '-d' flag if you want to remove this submodule and all the submodules the depends on it
+        EOM
+      end
+
+      it "should not raise an error if ran with the -d flag" do
+        subject.start(["submodules", "deactivate", "pathogen", "-d"]).should puts <<-EOM
+The following submodules has been deactivated, please run 'vimius update'
+- command-t
+- github
+- tlib
+- pathogen
+        EOM
+      end
     end
   end
 end

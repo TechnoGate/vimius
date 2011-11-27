cde2124b6d759b5bd7d1176c6cc93ff0430106d4
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-27 15:27:18 +0100

    Submodules spec: Add full tests for the functionality

--- spec/lib/vimius/command/submodules_spec.rb
+++ spec/lib/vimius/command/submodules_spec.rb
@@ -5,6 +5,14 @@ module Command
     subject { TechnoGate::TgCli::Main }
 
     context "#list" do
+      it "should mark active submodules with ' |== '" do
+        subject.start(["submodules", "list"]).should puts " |== pathogen"
+      end
+
+      it "should mark inactive submodules with ' |-- '" do
+        subject.start(["submodules", "list"]).should puts " |-- command-t"
+      end
+
       it "should be able to list submodules by group" do
         subject.start(["submodules", "list"]).should puts <<-EOM
 core
@@ -16,13 +24,7 @@ tools
 
 Active submodules are prefixed with the '|==' marker
         EOM
-
       end
-
-      it "should mark active submodules with ' |== '" do
-      end
-
-      it "should mark inactive submodules with ' |-- '"
     end
 
     context "#activate" do

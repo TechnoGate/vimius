298d28cb17baa6201e012699386a423e2740373c
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-28 16:30:23 +0100

    UpdateCommand: Finish up.

--- lib/vimius/command/update.rb
+++ lib/vimius/command/update.rb
@@ -1,38 +1,52 @@
 # -*- encoding: utf-8 -*-
 
-if false
-  module TechnoGate
-    module Vimius
-      module CLI
-        module Update
+module TechnoGate
+  module Vimius
+    module Command
+      class Update < TgCli::Base
+        register "update", "Install & Update Janus plugins"
 
-          def self.included(base)
-            base.class_eval <<-END, __FILE__, __LINE__ + 1
-              desc "update", "Update vimius"
-              def update
-                # Sanity check
-                sanity_check_update
-                # Fetch the active submodules
-                active_submodules = Vimius.submodules.active_by_group
-                # Go into the vimius folder
-                Dir.chdir(USER_VIM_PATH) do
-                  # Update the active submodules
-                  active_submodules.each do |group, submodules|
-                    submodules.each do |submodule|
-                      Shell.exec "git submodule update --init -- \#{submodule[:path]}"
-                    end
-                  end
-                end
-              end
-
-              protected
-              def sanity_check_update
-                unless File.exists?(USER_VIM_PATH) && File.exists?(MODULES_FILE)
-                  abort "Vimius is not installed, please run 'vimius install'"
-                end
-              end
-            END
+        def sanity_check
+          unless File.exists?(USER_VIM_PATH) && File.exists?(MODULES_FILE)
+            abort "Vimius is not installed, please run 'vimius install'"
           end
+        end
+
+        def submodules
+          Vimius.submodules.active.each do |submodule|
+            Shell.exec "cd #{USER_VIM_PATH}"
+            Shell.exec "git submodule init -- #{submodule[:path]}"
+            Shell.exec "git submodule sync -- #{submodule[:path]}"
+            Shell.exec "git submodule update -- #{submodule[:path]}"
+          end
+        end
+
+        def janus
+          Shell.exec <<-EOS
+# Source RVM so janus .rvmrc takes effect
+[[ -s /usr/local/rvm/scripts/rvm ]] && source /usr/local/rvm/scripts/rvm
+[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
+
+# Source Rbenv so janus .rbenv-version takes effect
+if ! type rbenv &> /dev/null; then
+  if [[ -s $HOME/.rbenv/bin/rbenv ]]; then
+    eval "`$HOME/.rbenv/bin/rbenv init -`"
+  fi
+fi
+
+cd #{USER_VIM_PATH}
+
+if [ ! -x `which gem 2>/dev/null` ]; then
+  echo "Rubygems is not installed please install it (using system ruby)
+  exit 1
+fi
+
+if [ ! -x `which gem 2>/dev/null` ]; then
+  echo "Rake is not installed please install it (using system ruby)
+fi
+
+rake install
+          EOS
         end
       end
     end
--- spec/lib/vimius/command/update_spec.rb
+++ spec/lib/vimius/command/update_spec.rb
@@ -1,59 +1,94 @@
-#require 'spec_helper'
+require 'spec_helper'
 
-#class CliUpdateTestClass < ::Thor
-  #include CLI::Update
-#end
+module Command
+  describe Update do
 
-#module CLI
-  #describe Update do
-    #subject { CliUpdateTestClass.new }
+    subject { TechnoGate::TgCli::Main }
 
-    #context "#update" do
-      #pending { should respond_to :update }
+    context "#sanity_check" do
+      before(:each) do
+        File.stubs(:exists?).with(USER_VIM_PATH).returns(false)
+        File.stubs(:exists?).with(MODULES_FILE).returns(false)
+      end
 
-      #pending "should call sanity check" do
-        #subject.expects(:sanity_check).once
+      it "should check that vimius is installed by checking USER_VIM_PATH" do
+        File.expects(:exists?).with(USER_VIM_PATH).returns(false).once
+        File.stubs(:exists?).with(MODULES_FILE).returns(true)
 
-        #subject.update
-      #end
+        capture(:stderr) { subject.start(["update"]) }
+      end
 
-      #pending "should get the list of active submodules" do
-        #Vimius.submodules.expects(:active_by_group).once
+      it "should check that vimius is installed by checking MODULES_FILE" do
+        File.stubs(:exists?).with(USER_VIM_PATH).returns(true)
+        File.expects(:exists?).with(MODULES_FILE).returns(false).once
 
-        #subject.update
-      #end
-    #end
+        capture(:stderr) { subject.start(["update"]) }
+      end
 
-    #context "#sanity_check" do
-      #pending { should respond_to :sanity_check}
+      it "should print an error" do
+        capture(:stderr) { subject.start(["update"]) }.
+          should puts("Vimius is not installed, please run 'vimius install'")
+      end
+    end
 
-      #pending "should check if USER_VIM_PATH exists" do
-        #::File.expects(:exists?).with(USER_VIM_PATH).returns(true).once
+    context "#submodules" do
+      it "should fetch the list of active submodules" do
+        Vimius.submodules.expects(:active).returns(expected_active_submodules).once
 
-        #subject.send :sanity_check
-      #end
+        capture(:stdout) { subject.start(["update"]) }
+      end
 
-      #pending "should check if MODULES_FILE exists" do
-        #::File.expects(:exists?).with(MODULES_FILE).returns(true).once
+      it "should run exec 3 times" do
+        Shell.expects(:exec).times(3)
 
-        #subject.send :sanity_check
-      #end
+        capture(:stdout) { subject.start(["update"]) }
+      end
 
-      #pending "should abort if vimius is not installed" do
-        #::File.stubs(:exists?).with(USER_VIM_PATH).returns(false)
+      it "should go into the folder" do
+        Shell.expects(:exec).with("cd #{USER_VIM_PATH}").times(3)
 
-        #subject.send :sanity_check
+        capture(:stdout) { subject.start(["update"]) }
+      end
 
-        #"Vimius is not installed, please run 'vimius install'".should be_in_output
-      #end
+      it "should run git submodule init" do
+        expected_active_submodules.each do |submodule|
+          Shell.expects(:exec).with("git submodule init -- #{submodule["path"]}").once
+        end
 
-      #pending "should abort if vimius is not installed" do
-        #::File.stubs(:exists?).with(MODULES_FILE).returns(false)
+        capture(:stdout) { subject.start(["update"]) }
+      end
+    end
 
-        #subject.send :sanity_check
+    context "#janus" do
+      it "should exec" do
+        Shell.expects(:exec).with(<<-EOS)
+# Source RVM so janus .rvmrc takes effect
+[[ -s /usr/local/rvm/scripts/rvm ]] && source /usr/local/rvm/scripts/rvm
+[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 
-        #"Vimius is not installed, please run 'vimius install'".should be_in_output
-      #end
-    #end
-  #end
-#end
+# Source Rbenv so janus .rbenv-version takes effect
+if ! type rbenv &> /dev/null; then
+  if [[ -s $HOME/.rbenv/bin/rbenv ]]; then
+    eval "`$HOME/.rbenv/bin/rbenv init -`"
+  fi
+fi
+
+cd #{USER_VIM_PATH}
+
+if [ ! -x `which gem 2>/dev/null` ]; then
+  echo "Rubygems is not installed please install it (using system ruby)
+  exit 1
+fi
+
+if [ ! -x `which gem 2>/dev/null` ]; then
+  echo "Rake is not installed please install it (using system ruby)
+fi
+
+rake install
+        EOS
+
+        capture(:stdout) { subject.start(["update"]) }
+      end
+    end
+  end
+end

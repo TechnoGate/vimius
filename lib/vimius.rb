ddbdc5ae080d6ec50cd625f34d3dff3c6f73f027
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-28 01:07:17 +0100

    Vimius: Define VIMIUS_ASCII_PATH

--- lib/vimius.rb
+++ lib/vimius.rb
@@ -17,6 +17,7 @@ USER_VIMIUS_PATH = File.join USER_VIM_PATH, 'janus'
 VIMIUS_PATH = File.expand_path(File.join File.dirname(__FILE__), '..')
 VIMIUS_LIB_PATH = File.join VIMIUS_PATH, 'lib'
 VIMIUS_SPEC_PATH = File.join VIMIUS_PATH, 'spec'
+VIMIUS_ASCII_PATH = File.join VIMIUS_LIB_PATH, 'vimius', 'vimius_ascii.txt'
 CONFIG_FILE = File.expand_path(File.join ENV['HOME'], '.vimius.yaml')
 MODULES_FILE = File.join(USER_VIMIUS_PATH, 'submodules.yaml')
 MODULES_CACHE_FILE = File.expand_path(File.join ENV['HOME'], '.vimius_cache.yaml')
